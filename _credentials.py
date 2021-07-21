19725d19ca8aa0e2589ebe5063a8546839dceedf
Jonas Materna <jonas.materna@web.de>
2021-07-21 15:45:46 +0200

    Credentials

---
+++ _credentials.py
@@ -0,0 +1,6 @@
+# ArgumenText API credentials
+# In order to run the code you need to regster with ArgumenText (https://api.argumentsearch.com/en/api_registration)
+# Copy the file _credentials.py to credentials.py in the project main directory. Edit it by adding your ArgumenText credentials.
+
+userID = "FILL IN"
+APIkey = "FILL IN"

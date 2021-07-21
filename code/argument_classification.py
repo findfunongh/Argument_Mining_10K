8ea820f4922af80c80af496797d29f3a956fa75f
Jonas Materna <jonas.materna@web.de>
2021-07-21 16:57:17 +0200

    First Code

--- code/argument_classification.py
+++ code/argument_classification.py
@@ -5,27 +5,22 @@ from datetime import datetime
 from credentials import userID, APIkey
 
 
+with open("data/Filing_1.txt") as f:
+    currFiling = " ".join(f.readlines())
 
 
-'''
+
 payload = {
-    "topic": "Brexit",
-    "text": ,
+    "topic": "tempo limit",
+    "text": currFiling,
     "predictStance": True,
     "computeAttention": True,
     "showOnlyArguments": False,
     "userID": userID,
-    "apiKey": APIkey
+    "apiKey": APIkey 
     }
-
-
-
-
-
-
 
 json_dict = requests.post("https://api.argumentsearch.com/en/classify", timeout=300, data=json.dumps(payload), headers={'Content-Type': 'application/json'}).json()
 
 print(json_dict)
 
-'''
